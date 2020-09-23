a4058c128701ed9048ec4dbca8887d0b6906b6ed
Sergey Kuznetsov <wildermind69@gmail.com>
2020-09-23 20:44:53 +0300

    Initial commit.

---
+++ Qooore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Qooore
+//
+//  Created by Sergey Kuznetsov on 23.09.2020.
+//  Copyright © 2020 Qooore Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
