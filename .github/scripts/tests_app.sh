ae6011b1abb4f7bc2dc56c722614cf5f9068f5d3
dvereshchagin <vereshhagin98@gmail.com>
2020-09-25 00:15:53 +0200

    t3

--- .github/scripts/tests_app.sh
+++ .github/scripts/tests_app.sh
@@ -4,5 +4,6 @@ set -eo pipefail
 
 xcodebuild -workspace Qooore.xcworkspace \
             -scheme Qooore\ iOS \
+            -UseModernBuildSystem=NO \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
