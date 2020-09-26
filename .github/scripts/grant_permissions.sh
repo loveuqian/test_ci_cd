bb6d837b4980880ea99123a5fe20e34927e88b74
dvereshchagin <vereshhagin98@gmail.com>
2020-09-26 22:40:41 +0200

    t34

--- .github/scripts/grant_permissions.sh
+++ .github/scripts/grant_permissions.sh
@@ -1,4 +1,4 @@
-chmod +rx tests_app.sh 
-chmod +rx clean_all.sh 
-chmod +rx archive_build.sh
-chmod +rx export_app.sh
+chmod +rx .github/scripts/tests_app.sh
+chmod +rx .github/scripts/clean_all.sh 
+chmod +rx .github/scripts/archive_build.sh
+chmod +rx .github/scripts/export_app.sh
